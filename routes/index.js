f0e889dae6d3243db2e6b9e90e33106730712578
Ryan Johnson <ryan@258webco.com>
2020-05-04 18:54:00 -0500

    Initial Setup.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  //   res.send("Hello Manny!!!");
+  res.render("index");
+});
+
+module.exports = router;
